cf7d59180ba2c3d1bc30e34e21e98761aa78b62e
Guilherme Nascimento <paitomax@gmail.com>
2022-07-15 02:48:46 -0300

    Initial unit tests

--- test/interleaved_builder_test.dart
+++ test/interleaved_builder_test.dart
@@ -1,5 +1,115 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:interleaved_builder/interleaved_builder.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockBuildContext extends Mock implements BuildContext {}
+
+class FakeBuildContext extends Fake implements BuildContext {}
 
 void main() {
-  test('', () {});
+  setUp(() {
+    registerFallbackValue(FakeBuildContext());
+  });
+
+  group('InterleavedBuilder', () {
+    test('should throw RangeError when index is out of range', () {
+      BuildContext context;
+
+      final itemBuilder = InterleavedBuilder(
+        step: 1,
+        itemLength: 10,
+        interleavedItemLength: 10,
+        itemBuilder: (context, interleaveIndex, listIndex) {
+          return 'item $interleaveIndex';
+        },
+        interleavedItemBuilder: (context, interleaveIndex, listIndex) {
+          return 'interleaved $interleaveIndex';
+        },
+      );
+
+      Object? error;
+      try {
+        itemBuilder.build(MockBuildContext(), -1);
+      } catch (e) {
+        error = e;
+      }
+      expect(error is RangeError, true);
+
+      error = null;
+
+      try {
+        itemBuilder.build(MockBuildContext(), 21);
+      } catch (e) {
+        error = e;
+      }
+
+      expect(error is RangeError, true);
+    });
+
+    test('should return item when is on initial offset', () {
+      BuildContext context;
+      const offset = 2;
+
+      final itemBuilder = InterleavedBuilder(
+        step: 1,
+        initialOffset: offset,
+        itemLength: 10,
+        interleavedItemLength: 10,
+        itemBuilder: (context, interleaveIndex, listIndex) {
+          return 'item $interleaveIndex';
+        },
+        interleavedItemBuilder: (context, interleaveIndex, listIndex) {
+          return 'interleaved $interleaveIndex';
+        },
+      );
+
+      for (var i = 0; i < offset; i++) {
+        expect(itemBuilder.build(MockBuildContext(), i), 'item $i');
+      }
+      expect(itemBuilder.build(MockBuildContext(), offset), 'interleaved 0');
+    });
+
+    test('initialOffset should be equal step', () {
+      BuildContext context;
+      // const offset = 2;
+      const step = 1;
+      final itemBuilder = InterleavedBuilder(
+        step: 1,
+        // initialOffset: offset,
+        itemLength: 10,
+        interleavedItemLength: 10,
+        itemBuilder: (context, interleaveIndex, listIndex) {
+          return 'item $interleaveIndex';
+        },
+        interleavedItemBuilder: (context, interleaveIndex, listIndex) {
+          return 'interleaved $interleaveIndex';
+        },
+      );
+
+      for (var i = 0; i < step; i++) {
+        expect(itemBuilder.build(MockBuildContext(), i), 'item $i');
+      }
+      expect(itemBuilder.build(MockBuildContext(), step), 'interleaved 0');
+    });
+
+    test('should start with interleaved item', () {
+      BuildContext context;
+      final itemBuilder = InterleavedBuilder(
+        step: 1,
+        initialOffset: 0,
+        itemLength: 10,
+        interleavedItemLength: 10,
+        itemBuilder: (context, interleaveIndex, listIndex) {
+          return 'item $interleaveIndex';
+        },
+        interleavedItemBuilder: (context, interleaveIndex, listIndex) {
+          return 'interleaved $interleaveIndex';
+        },
+      );
+
+      expect(itemBuilder.build(MockBuildContext(), 0), 'interleaved 0');
+      expect(itemBuilder.build(MockBuildContext(), 1), 'item 0');
+    });
+  });
 }
